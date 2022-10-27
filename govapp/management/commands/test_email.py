62768ee3e6b135cc321ea95dafba7545f5ddb172
HaydenGaia <hayden.richards@gaiaresources.com.au>
2022-10-27 13:54:35 +0800

    Fix linting/type-checking errors in test_email.py

--- govapp/management/commands/test_email.py
+++ govapp/management/commands/test_email.py
@@ -1,25 +1,52 @@
+"""Django management command to send a test email."""
+
+
+# Standard
+import argparse
+
+# Third-Party
+from django.core.management import base
+
+# Local
 from govapp import emails
-from django.core.management.base import BaseCommand
-from django.conf import settings
-from confy import env, database
-import requests
-import json
 
-class Command(BaseCommand):
-    help = 'Test email'
+# Typing
+from typing import Any
 
-    def add_arguments(self, parser):
-          parser.add_argument('-t' '--to', type=str , help='To Email Address')
 
-    def handle(self, *args, **options):
-         to_arg = options['t__to']
-         # add variables into the email.
-         context= {'test' :  'test'}
-         cc = None
-         bcc = None
-         from_email='no-reply@dbca.wa.gov.au'
-         template_group='pvs'
+class TestEmailCommand(base.BaseCommand):
+    """Management command to send a test email."""
+    # Help string for the command-line interface
+    help = "Test email"  # noqa: A003
 
-         emails.sendHtmlEmail([to_arg],'Test Email',context,'email/test-body.html',cc,bcc,from_email,template_group,attachments=None)   
+    def add_arguments(self, parser: argparse.ArgumentParser) -> None:
+        """Adds command-line arguments to the Django management command.
 
-         
+        Args:
+            parser (argparse.ArgumentParser): Argument parser to add to.
+        """
+        # Add arguments
+        parser.add_argument("-t", "--to", type=str, help="To Email Address")
+
+    def handle(self, *args: Any, **options: Any) -> None:
+        """Handles the Django management command functionality.
+
+        Args:
+            *args (Any): Positional arguments passed to the command.
+            **options (Any): Keyword arguments passed to the command.
+        """
+        # Retrieve the "to" argument
+        to_arg = options["to"]
+
+        # Send email
+        emails.sendHtmlEmail(
+            to=[to_arg],
+            subject="Test Email",
+            context={"test": "test"},
+            template="email/test-body.html",
+            cc=None,
+            bcc=None,
+            from_email="no-reply@dbca.wa.gov.au",
+            template_group="pvs",
+            attachments=None,
+        )
