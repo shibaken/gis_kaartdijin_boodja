b470514a748168481d855a165f237f1c6d747bce
Jason Moore <jason.moore@dbca.wa.gov.au>
2023-06-09 15:47:23 +0800

    AdminUI Search for LayerSubmissions

--- govapp/apps/catalogue/admin.py
+++ govapp/apps/catalogue/admin.py
@@ -25,12 +25,15 @@ class EmailNotificationAdmin(reversion.admin.VersionAdmin):
 class LayerSymbologyAdmin(reversion.admin.VersionAdmin):
     search_fields = ('catalogue_entry__id','sld')
 
+class LayerSubmission(reversion.admin.VersionAdmin):
+    search_fields = ('id','catalogue_entry__name','description')
+
 # Register Models
 admin.site.register(models.catalogue_entries.CatalogueEntry, CatalogueEntryAdmin)
 admin.site.register(models.custodians.Custodian, CustodianAdmin)
 admin.site.register(models.layer_attributes.LayerAttribute, reversion.admin.VersionAdmin)
 admin.site.register(models.layer_metadata.LayerMetadata, reversion.admin.VersionAdmin)
-admin.site.register(models.layer_submissions.LayerSubmission, reversion.admin.VersionAdmin)
+admin.site.register(models.layer_submissions.LayerSubmission, LayerSubmission)
 admin.site.register(models.layer_subscriptions.LayerSubscription, reversion.admin.VersionAdmin)
 admin.site.register(models.layer_symbology.LayerSymbology, LayerSymbologyAdmin)
 admin.site.register(models.notifications.EmailNotification, EmailNotificationAdmin)
