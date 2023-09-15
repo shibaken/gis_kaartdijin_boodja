3510bc5d7cc5df68d21498d62f5146982aa17036
Changdae Jung <akchuimy@gmail.com>
2023-09-15 14:25:50 +0800

    add GeoServer Queue Cron Job on management command

--- govapp/static/js/kbmanagementcommands.js
+++ govapp/static/js/kbmanagementcommands.js
@@ -2,6 +2,7 @@ var kbmanagementcommands = {
     var: {
         "scan_url" :"/api/management/commands/scan_dir/",
         "get_sharepoint_submission_url" :"/api/management/commands/get_sharepoint_submissions/",
+        "geoserver_queue_cron_job" :"/api/management/commands/excute_geoserver_queue/",
     },
     run_scanner: function() {
             console.log("RUNNING SCANNER")
@@ -53,6 +54,31 @@ var kbmanagementcommands = {
             },
         });
     },
+    run_geoserver_queue_cron_job: function() {
+        console.log("RUNNING GEOSERVER QUEUE CRON JOB")
+        $('#geoserver-queue-job-response-success').html('');
+        $('#geoserver-queue-job-response-error').html('');
+        var csrf_token = $("#csrfmiddlewaretoken").val();
+        $('#run-geoserver-queue').attr('disabled','disabled');
+        $('#run-geoserver-queue-loader').show();
+        $.ajax({
+            url: kbmanagementcommands.var.geoserver_queue_cron_job,
+            type: 'POST',
+            headers: {'X-CSRFToken' : csrf_token},
+            contentType: 'application/json',
+            success: function (response) {                            
+                $('#geoserver-queue-job-response-success').html("Completed");                   
+                $('#run-geoserver-queue').removeAttr('disabled');
+                $('#run-geoserver-queue-loader').hide();
+                
+            },
+            error: function (error) {                    
+                $('#geoserver-queue-job-response-error').html("Error running job");
+                $('#run-geoserver-queue').removeAttr('disabled');
+                $('#run-geoserver-queue-loader').hide();
+            },
+        });
+    },
     init: function() {
         $( "#run-scanner" ).click(function() {
             console.log("Running Scanner");
@@ -64,8 +90,15 @@ var kbmanagementcommands = {
             
             kbmanagementcommands.run_sharepoint_submissions();
         });
+
+        $( "#run-geoserver-queue" ).click(function() {
+            console.log("Running Scanner");
+            
+            kbmanagementcommands.run_geoserver_queue_cron_job();
+        });
         $('#run-scanner-loader').hide();
         $('#run-sharepoint-scanner-loader').hide();
+        $('#run-geoserver-queue-loader').hide();
     }
 
 }
