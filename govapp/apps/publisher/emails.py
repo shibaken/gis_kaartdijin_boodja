69e865da2ef9bb2e5358b49d33d5dbc2f03827d0
Changdae Jung <akchuimy@gmail.com>
2023-09-15 13:05:06 +0800

    add prefix '[KB]' to email subject of publisher

--- govapp/apps/publisher/emails.py
+++ govapp/apps/publisher/emails.py
@@ -7,20 +7,20 @@ from govapp.apps.emails import emails
 
 class PublishEntryLockedEmail(emails.TemplateEmailBase):
     """Publish Entry Locked Email Abstraction."""
-    subject = "Kaartdijin Boodja Publish Entry locked"
+    subject = "[KB] Kaartdijin Boodja Publish Entry locked"
     html_template = "publish_entry_locked_email.html"
     txt_template = "publish_entry_locked_email.txt"
 
 
 class PublishEntryPublishSuccessEmail(emails.TemplateEmailBase):
     """Publish Entry Publish Success Email Abstraction."""
-    subject = "Kaartdijin Boodja Publish Entry published"
+    subject = "[KB] Kaartdijin Boodja Publish Entry published"
     html_template = "publish_entry_publish_success_email.html"
     txt_template = "publish_entry_publish_success_email.txt"
 
 
 class PublishEntryPublishFailEmail(emails.TemplateEmailBase):
     """Publish Entry Publish Fail Email Abstraction."""
-    subject = "Kaartdijin Boodja Publish Entry failed to publish"
+    subject = "[KB] Kaartdijin Boodja Publish Entry failed to publish"
     html_template = "publish_entry_publish_fail_email.html"
     txt_template = "publish_entry_publish_fail_email.txt"
