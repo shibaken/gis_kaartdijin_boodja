efe9171e709be0bc190e464fc0ecb928e003eddc
katsufumi shibata <katsufumi.shibata@dbca.wa.gov.au>
2024-08-06 15:18:43 +0800

    Tweak UI

--- govapp/apps/catalogue/admin.py
+++ govapp/apps/catalogue/admin.py
@@ -9,7 +9,9 @@ from django.utils.html import format_html
 
 # Local
 from govapp.apps.catalogue import models
+from govapp.apps.catalogue.models.catalogue_entries import CatalogueEntryStatus
 from govapp.apps.catalogue.models.layer_submissions import LayerSubmissionStatus
+from govapp.apps.catalogue.models.layer_subscriptions import LayerSubscriptionStatus, LayerSubscriptionType
 
 # class CatalogueModelAdmin(reversion.admin.VersionAdmin):
 #     ordering = ('id', 'name')
@@ -28,12 +30,31 @@ class CatalogueEntryAdmin(reversion.admin.VersionAdmin):
     # See: https://stackoverflow.com/questions/5385933/a-better-django-admin-manytomany-field-widget
     # filter_horizontal = ["editors"]
     search_fields = ('id', 'name', 'assigned_to__email', 'custodian__name', 'layer_subscription__name',)
-    list_display = ('id', 'name', 'status', 'type', 'layer_subscription_link','custodian_link', 'assigned_to_link', 'created_at', 'updated_at')
-    list_filter = ('status', 'type', 'assigned_to', 'custodian',)
+    # list_display = ('id', 'name', 'status', 'type', 'permission_type', 'layer_subscription_link','custodian_link', 'assigned_to_link', 'created_at', 'updated_at')
+    list_display = ('id', 'name', 'get_status', 'type', 'permission_type', 'layer_subscription_link','custodian_link', 'assigned_to_link', 'created_at', 'updated_at')
+    list_filter = ('status', 'type', 'assigned_to', 'custodian', 'permission_type')
     list_display_links = ('id', 'name',)
     ordering = ('id',)
     inlines = [CatalogueEntryPermissionInline]
     raw_id_fields = ('custodian', 'assigned_to')
+
+    class Media:
+        css = {'all': ('common/css/admin_custom.css',)}
+
+    def get_status(self, obj):
+        if obj.status == CatalogueEntryStatus.NEW_DRAFT:
+            return format_html('<span class="badge badge-pill bg-secondary">New Draft</span>')
+        elif obj.status == CatalogueEntryStatus.LOCKED:
+            return format_html('<span class="badge badge-pill bg-success">Locked</span>')
+        elif obj.status == CatalogueEntryStatus.DECLINED:
+            return format_html('<span class="badge badge-pill bg-danger">Declined</span>')
+        elif obj.status == CatalogueEntryStatus.DRAFT:
+            return format_html('<span class="badge badge-pill bg-secondary">Draft</span>')
+        elif obj.status == CatalogueEntryStatus.PENDING:
+            return format_html('<span class="badge badge-pill bg-warning">Pending</span>')
+        else:
+            return ''
+    get_status.short_description = 'Status'
     
     def custodian_link(self, obj):
         return format_html(f'<a href="/admin/catalogue/custodian/{obj.custodian.id}/change/">{obj.custodian.name}</a>') if obj.custodian else '-'
@@ -85,27 +106,37 @@ class LayerSubmissionAdmin(reversion.admin.VersionAdmin):
     ordering = ('id',)
     raw_id_fields = ('catalogue_entry',)
 
+    class Media:
+        css = {'all': ('common/css/admin_custom.css',)}
+
     def catalogue_entry_link(self, obj):
         return construct_catalogue_entry_link(obj.catalogue_entry)
     catalogue_entry_link.short_description = 'Catalogue Entry'
 
     def coloured_status(self, obj):
         if obj.status == LayerSubmissionStatus.SUBMITTED:
-            return format_html('<span style="color: gray;">{}</span>', obj.get_status_display())
+            # return format_html('<span style="color: gray;">{}</span>', obj.get_status_display())
+            return format_html('<span class="badge badge-pill bg-secondary">' + obj.get_status_display() + '</span>')
         elif obj.status == LayerSubmissionStatus.ACCEPTED:
-            return format_html('<span style="color: green;">{}</span>', obj.get_status_display())
+            # return format_html('<span style="color: green;">{}</span>', obj.get_status_display())
+            return format_html('<span class="badge badge-pill bg-success">' + obj.get_status_display() + '</span>')
         elif obj.status == LayerSubmissionStatus.DECLINED:
-            return format_html('<span style="color: #dc3545;">{}</span>', obj.get_status_display())
+            # return format_html('<span style="color: #dc3545;">{}</span>', obj.get_status_display())
+            return format_html('<span class="badge badge-pill bg-danger">' + obj.get_status_display() + '</span>')
         else:
             return '---'
+    coloured_status.short_description = 'Status'
 
 
 class LayerSubscriptionAdmin(reversion.admin.VersionAdmin):
     search_fields = ('id', 'name', 'description', 'username', 'url', 'host',)
-    list_display = ('id', 'name', 'type', 'description', 'status', 'enabled', 'workspace_link', 'assigned_to_link', 'username', 'url_link', 'host', 'max_connections', 'min_connections', 'updated_at', 'created_at')
+    list_display = ('id', 'name', 'type', 'description', 'get_status', 'enabled', 'workspace_link', 'assigned_to_link', 'username', 'url_link', 'host', 'max_connections', 'min_connections', 'updated_at', 'created_at')
     list_filter = ('enabled', 'type', 'status', 'workspace', 'assigned_to',)
     list_display_links = ('id', 'name',)
     ordering = ('id',)
+
+    class Media:
+        css = {'all': ('common/css/admin_custom.css',)}
 
     def assigned_to_link(self, obj):
         return format_html(f'<a href="/admin/auth/user/{obj.assigned_to.id}/change">{obj.assigned_to}</a>') if obj.assigned_to else '-'
@@ -118,6 +149,21 @@ class LayerSubscriptionAdmin(reversion.admin.VersionAdmin):
     def workspace_link(self, obj):
         return format_html(f'<a href="/admin/publisher/workspace/{obj.workspace.id}/change/">{obj.workspace.name}</a>')
     workspace_link.short_description = 'Workspace'
+
+    def get_status(self, obj):
+        if obj.status == LayerSubscriptionStatus.NEW_DRAFT:
+            return format_html('<span class="badge badge-pill bg-secondary">' + obj.get_status_display() + '</span>')
+        elif obj.status == LayerSubscriptionStatus.LOCKED:
+            return format_html('<span class="badge badge-pill bg-success">' + obj.get_status_display() + '</span>')
+        elif obj.status == LayerSubscriptionStatus.DECLINED:
+            return format_html('<span class="badge badge-pill bg-danger">' + obj.get_status_display() + '</span>')
+        elif obj.status == LayerSubscriptionStatus.DRAFT:
+            return format_html('<span class="badge badge-pill bg-secondary">' + obj.get_status_display() + '</span>')
+        elif obj.status == LayerSubscriptionStatus.PENDING:
+            return format_html('<span class="badge badge-pill bg-warning">' + obj.get_status_display() + '</span>')
+        else:
+            return '---'
+    get_status.short_description = 'Status'
 
     
 class LayerSymbologyAdmin(reversion.admin.VersionAdmin):
