d3c2fab31cd086aabea769e5bd4fb0078c7a7aeb
katsufumi shibata <katsufumi.shibata@dbca.wa.gov.au>
2024-12-06 15:42:50 +0800

    Allow to upload uncompressed geojson file by configuring ALLOWED_EXTENSIONS_TO_UPLOAD env variable.  Default to .7z and .zip

--- govapp/gis/readers/formats/geojson.py
+++ govapp/gis/readers/formats/geojson.py
@@ -30,8 +30,9 @@ class GeoJSONReader(base.LayerReader):
             bool: Whether this file is compatible with this reader.
         """
         # Check and Return
-        # if file.is_file() and file.suffix.lower() in (".json", ".geojson"):
-        #     return True
+        if file.is_file() and file.suffix.lower() in (".json", ".geojson"):
+            if cls.name_property_exists(file):
+                return True
         if file.is_dir() and utils.exists(file.glob("*.json")) or file.is_dir() and utils.exists(file.glob("*.geojson")):
             if cls.geojson_has_name_property(file):
                 return True
@@ -42,14 +43,18 @@ class GeoJSONReader(base.LayerReader):
         """Determines whether the GeoJSON file has a name property."""
         if cls.contain_single_geojson(path_to_folder):
             geojson_files = cls.get_geojson_files(path_to_folder)
-            with open(geojson_files[0]) as f:
-                data = json.load(f)
-                if 'name' in data:
-                    return True
-                logger.error(f'The GeoJSON file does not have a name property at the top level: [{geojson_files[0]}]')
-                raise ValueError(f'The GeoJSON file does not have a name property at the top level: [{geojson_files[0]}]')
+            return cls.name_property_exists(geojson_files[0])
         logger.error(f'There are more than one GeoJSON files in the folder: [{path_to_folder}]')
         raise ValueError(f'There are more than one GeoJSON files in the folder: [{path_to_folder}]')
+
+    @classmethod
+    def name_property_exists(cls, geojson_file):
+        with open(geojson_file) as f:
+            data = json.load(f)
+            if 'name' in data:
+                return True
+            logger.error(f'The GeoJSON file does not have a name property at the top level: [{geojson_file}]')
+            raise ValueError(f'The GeoJSON file does not have a name property at the top level: [{geojson_file}]')
 
     @classmethod
     def contain_single_geojson(cls, path_to_folder):
@@ -58,7 +63,9 @@ class GeoJSONReader(base.LayerReader):
     
     @classmethod
     def get_geojson_files(cls, path_to_folder):
-        """Returns a list of GeoJSON files in a folder."""
+        """ Returns a list of GeoJSON files in a folder."""
+        if path_to_folder.is_file() and path_to_folder.suffix.lower() in (".json", ".geojson"):
+            return [path_to_folder]
         json_files = list(path_to_folder.glob("*.json"))
         geojson_files = list(path_to_folder.glob("*.geojson"))
         return json_files + geojson_files
