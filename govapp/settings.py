671d183202ac3ed904c41115fac83025df99f404
katsufumi shibata <katsufumi.shibata@dbca.wa.gov.au>
2025-06-05 09:52:23 +0800

    Turn off the django rest framework api pages.  (Allow JSONRenderer only)

--- govapp/settings.py
+++ govapp/settings.py
@@ -180,6 +180,9 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 100,
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
 }
 
 # DRF Spectacular Settings
