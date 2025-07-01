9d8d2ec474e8012395bd5ec48111417593ef9b11
katsufumi shibata <katsufumi.shibata@dbca.wa.gov.au>
2025-07-01 12:08:38 +0800

    Allow user to upload .gpkg file

--- govapp/settings.py
+++ govapp/settings.py
@@ -404,7 +404,7 @@ CATALOGUE_ENTRY_TYPE_TO_DISPLAY = decouple.config("CATALOGUE_ENTRY_TYPE_TO_DISPL
                                                                                                             # SUBSCRIPTION_POSTGIS = 4
                                                                                                             # SUBSCRIPTION_QUERY = 5
 CDDP_USER_PATH = decouple.config("CDDP_USER_PATH", "V:/GIS1-Corporate/Data/")
-ALLOWED_EXTENSIONS_TO_UPLOAD = decouple.config("ALLOWED_EXTENSIONS_TO_UPLOAD", ".zip,.7z,.tar,.rar,.geojson,.json,.tif,.tiff").split(',')
+ALLOWED_EXTENSIONS_TO_UPLOAD = decouple.config("ALLOWED_EXTENSIONS_TO_UPLOAD", ".zip,.7z,.tar,.rar,.geojson,.json,.tif,.tiff,.gpkg").split(',')
 
 
 if not RUNNING_DEVSERVER and SENTRY_DSN and EMAIL_INSTANCE:
