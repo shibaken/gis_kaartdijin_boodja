432210e3cb373d6fb583a7bd4685ddfb235f1b6d
katsufumi shibata <katsufumi.shibata@dbca.wa.gov.au>
2025-08-20 12:57:26 +0800

    Add permission check to APIs for accessing logs

--- govapp/views.py
+++ govapp/views.py
@@ -14,6 +14,7 @@ from django.http import JsonResponse
 from rest_framework.decorators import permission_classes
 from owslib.wms import WebMapService
 from django.contrib.auth.mixins import UserPassesTestMixin
+from django.contrib.auth.decorators import login_required, user_passes_test
 
 # Internal
 from govapp import settings
@@ -684,6 +685,8 @@ def tail_lines(file_path, lines=1000, block_size=1024):
         return [line.decode('utf-8', errors='replace') for line in all_lines[-lines:]]
 
 
+@login_required # Ensures the user is logged in before any other checks.
+@user_passes_test(utils.user_can_view_logs) # Applies custom permission check.
 def get_logs(request):
     """
     API endpoint that returns log updates based on the given offset.
